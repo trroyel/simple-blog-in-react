2717bb7e0cb415f7c1f5092fe476c5a4a97734e4
trroyel <t.r.royel@gmail.com>
2018-06-29 13:59:25 +0600

    Add Switch components for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -27,8 +27,12 @@ class Blog extends Component {
                 <Route path="/"  render={() => <h1> How are you</h1>} /> */}
 
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                {/*Switch Render one Route at a time orderly.*/}
+                <Switch>
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+
             </div>
         );
     }
